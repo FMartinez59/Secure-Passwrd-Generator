a439b45daeb3c6374c03b45b62ecc3911ca6fcc3
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-03-16 14:06:27 -0400

    added password min-max& a library for the computer to choose from  but button does not work

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var generatePassword = Math.floor(Math.random() * passwordLibrary.length);
+var passwordMin = 8;
+var passwordMax = 128;
+var passwordLibrary = { 
+  numbers: '0123456789',
+  special: '!@#$%^&*()_+-=<>?.,', 
+  upperCase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  lowerCase: 'abcdefghijklmnopqrstuvwxyz'
+};
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
+ 
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword) {
+  window.prompt(' How many characters would you like?')
+
+}
+
+
