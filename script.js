e0ba8217181b9ce84de5cd12f2f07509a4b8495b
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-03-17 18:42:56 -0400

    added return for failed critireia and seperated libraray variables into an array

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ var generateBtn = document.querySelector("#generate");
 var passwordMin = 8;
 var passwordMax = 128;
 var passwordLibrary = { 
-  numbers: '0123456789',
-  special: '!@#$%^&*()_+-=<>?.,', 
-  upperCase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 
+  special: ['!', '@', '#', '$', '%', '^', '&', '*','(', ')', '_', '+', '-', '=','<', '>', '?', '.'],  
+  upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] ,
   lowerCase: 'abcdefghijklmnopqrstuvwxyz'
 };
 
@@ -17,9 +17,9 @@ var passwordLibrary = {
 function generateAskQuestions() {
   var passwordLength= prompt("Please enter how many characters you would like") ;
   console.log(passwordLength) 
-  if (passwordLength < 8 || passwordLength >128 ) {
+  if (passwordLength < 8 || passwordLength > 128 ) {
     alert("Must be more than 8 and no greater than 128")
-    generateAskQuestions()  
+    return;
   } 
   window.confirm("Would you like to include numbers in your password")
     console.log(passwordLibrary.numbers)
@@ -32,9 +32,11 @@ function generateAskQuestions() {
 
   window.confirm("Would you like lowercase letters?")
     console.log(passwordLibrary.lowerCase)
-  
+ 
+    writePassword()
 } 
 
+//store answers in a global scale
 
 
 
@@ -49,8 +51,6 @@ var password = generatePassword();
   passwordText.value = password;
 
 }
-
-
 
 
 // Add event listener to generate button
