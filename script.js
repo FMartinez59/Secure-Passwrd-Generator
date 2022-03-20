7c9c1cf5dd98bca9aeedb9455fb9db5b3c17e506
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-03-20 14:11:34 -0400

    defined user data now need to make password generator work

--- script.js
+++ script.js
@@ -6,32 +6,44 @@ var passwordMax = 128;
 var passwordLibrary = { 
   numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 
   special: ['!', '@', '#', '$', '%', '^', '&', '*','(', ')', '_', '+', '-', '=','<', '>', '?', '.'],  
-  upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] ,
+  upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
   lowerCase: ['a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
 }
 
-// var generatePassword = Math.floor(Math.random() * passwordLibrary.length);
+var passwordLength = []
+
+
+
+ var generatePassword = Math.floor(Math.random() * passwordLibrary.length);
+
+function generatePassword ()
+
+
+
+
+
+
 
 
 //this is declaring the function
 function generateAskQuestions() {
-  var passwordLength= prompt("Please enter how many characters you would like") ;
+ passwordLength = window.prompt("Please enter how many characters you would like") ;
   console.log(passwordLength) 
   if (passwordLength < 8 || passwordLength > 128 ) {
     alert("Must be more than 8 and no greater than 128")
     return;
   } 
-  window.confirm("Would you like to include numbers in your password")
-    console.log(passwordLibrary.numbers)
+  numbers = window.confirm("Would you like to include numbers in your password");
+    console.log(passwordLibrary.numbers);
 
-  window.confirm("Would you like Special characters?")
-    console.log(passwordLibrary.special)
+  special = window.confirm("Would you like Special characters?");
+    console.log(passwordLibrary.special);
 
-  window.confirm("Would you like uppercase letters?")
-    console.log(passwordLibrary.upperCase)
+  upperCase = window.confirm("Would you like uppercase letters?");
+    console.log(passwordLibrary.upperCase);
 
-  window.confirm("Would you like lowercase letters?")
-    console.log(passwordLibrary.lowerCase)
+  lowerCase = window.confirm("Would you like lowercase letters?");
+    console.log(passwordLibrary.lowerCase);
  
     writePassword()
 } 
