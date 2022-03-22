31e9bb7f01c06a682eb55ef6bbad8b81eaf422fb
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-03-21 22:10:18 -0400

    changed code back to where it works and added do while loop

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ var generateBtn = document.querySelector("#generate");
 
 var passwordMin = 8;
 var passwordMax = 128;
-var passwordLibrary = { 
-  numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 
-  special: ['!', '@', '#', '$', '%', '^', '&', '*','(', ')', '_', '+', '-', '=','<', '>', '?', '.'],  
+var passwordLibrary = {
+  numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  special: ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '<', '>', '?', '.'],
   upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-  lowerCase: ['a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+  lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
 }
 
 // let passwordLength = 0;
@@ -15,62 +15,56 @@ let numbers = false;
 let special = false;
 let upperCase = false;
 let lowerCase = false;
-
+let validChar = []
 var passwordLength = 'userInput'
 
- var generatePassword = Math.floor(Math.random() * passwordLibrary.length);
+// var generatePassword = Math.floor(Math.random() * passwordLibrary.length);
 
-function generatePassword () {
+function generatePassword() {
   //if else so if everything is true make password with all
-  userInput = parseInt(prompt("Please enter how many characters you would like"));
- 
+  generateAskQuestions()
+}
+//this is declaring the function
+function generateAskQuestions() {
+  var passwordLength = prompt("Please enter how many characters you would like") ;
+  console.log(passwordLength)
   if (passwordLength < 8 || passwordLength > 128 ) {
     alert("Must be more than 8 and no greater than 128")
     return;
-  } 
-  numbers = window.confirm("Would you like to include numbers in your password");
-    console.log(passwordLibrary.numbers);
+  }
+  do {
+  numb = window.confirm("Would you like to include numbers in your password")
+    console.log(passwordLibrary.numbers)
+  specialChar = window.confirm("Would you like Special characters?")
+    console.log(passwordLibrary.special)
+  uppChar = window.confirm("Would you like uppercase letters?")
+    console.log(passwordLibrary.upperCase)
+  lowerChar = window.confirm("Would you like lowercase letters?")
+    console.log(passwordLibrary.lowerCase)
+    
+    if (!numb && !specialChar && !upperCase && !lowerCase) { 
+      window.alert ('You need to have at least one option selected')
+    }
 
-  special = window.confirm("Would you like Special characters?");
-    console.log(passwordLibrary.special);
-
-  upperCase = window.confirm("Would you like uppercase letters?");
-    console.log(passwordLibrary.upperCase);
-
-  lowerCase = window.confirm("Would you like lowercase letters?");
-    console.log(passwordLibrary.lowerCase);
-
-    writePassword()
-} 
-
-
-
-
-
-
-
-
-//this is declaring the function
-// function generateAskQuestions() {
- passwordLength = window.prompt("Please enter how many characters you would like") ;
-  console.log(passwordLength) 
- 
-
+  }while (
+    !numb && !specialChar && !upperCase && !lowerCase
+    
+    )
+}
 
 
 // Write password to the #password input
 function writePassword() {
-var password = generatePassword();
-
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
- if (password !== undefined) {
-   passwordText.value = password;
- }
+
+  passwordText.value = password;
+
 }
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-  //this is calling the function
-  generatePassword()
+//this is calling the function
+
 
